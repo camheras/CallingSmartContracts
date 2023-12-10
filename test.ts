95a22bb96701d356d852d83bd70f1f0441c900e5
camheras <camheras@gmail.com>
2023-12-10 16:22:06 +0100

    add favicon

--- test.ts
+++ test.ts
@@ -3,6 +3,7 @@ import path from "path"
 
 const app = express()
 const port = 3000
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"))
