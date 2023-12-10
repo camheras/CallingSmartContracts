113fc21b210db10a44ba33e3610a1454c088b524
camheras <camheras@gmail.com>
2023-12-10 16:25:56 +0100

    change paths

--- test.ts
+++ test.ts
@@ -3,7 +3,6 @@ import path from "path"
 
 const app = express()
 const port = 3000
-app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"))
